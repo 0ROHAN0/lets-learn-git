cafd55669c1769ba0fa88492b95333d3a174d661
Rohan Kumar Upadhyay <rohanupadhyay175@gmail.com>
2021-09-01 17:42:13 +0530

    initial commit

---
+++ Luis.java
@@ -0,0 +1,11 @@
+public class Luis()
+{
+  public static void main(String[] args) {
+    System.out.println{"My learninhg is on"};
+    method_1();//comment
+    System.out.println("5+5");
+  }
+    method_1(){
+    System.out.println("mathod is called");
+    }
+}
---
+++ Luis.js
@@ -0,0 +1,7 @@
+function myfunc(){
+
+};
+
+setTimeout(function () {
+
+}, 10);
